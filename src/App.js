9072c5cef29029dbc174c49e3d9c70e6213c2457
SchwSimon <schwsimon.ss@gmail.com>
2019-10-26 13:56:30 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import CompA from './lib/CompA';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+
+          <CompA />
+        </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/lib/CompA.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './styles/styles.scss';
+
+export default class CompA extends Component {
+  static propTypes = {
+    text: PropTypes.string
+  }
+
+  render() {
+    return (
+      <div className='test'>
+        CompA
+      </div>
+    )
+  }
+}
