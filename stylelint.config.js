ca262363d9b51d1325153ba516088651a78666e3
SchwSimon <schwsimon.ss@gmail.com>
2019-11-02 19:34:16 +0100

    update config

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,5 +2,10 @@ module.exports = {
     extends: [
         "stylelint-config-recommended",
         "stylelint-config-standard"
-    ]
+    ],
+    rules: {
+        "indentation": 4,
+        "max-empty-lines": 2,
+        "color-hex-case": null
+    }
 };
