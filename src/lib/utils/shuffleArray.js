5fd690b45d078c110e9d8c914389e9880086aed3
SchwSimon <schwsimon.ss@gmail.com>
2020-04-11 18:43:15 +0200

    add shuffle array utility

---
+++ src/lib/utils/shuffleArray.js
@@ -0,0 +1,15 @@
+
+/**
+ * Randomly shuffle an array
+ * @param {Array} array
+ * @returns {Array}
+ */
+export function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return array;
+}
