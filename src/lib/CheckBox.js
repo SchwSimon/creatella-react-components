ee58f55e756add7fe2525471055f5c927ff23ba1
SchwSimon <schwsimon.ss@gmail.com>
2020-05-08 17:37:37 +0200

    classify

--- src/lib/CheckBox.js
+++ src/lib/CheckBox.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { classify } from './utils/classify';
 
 export default class CheckBox extends PureComponent {
     static propTypes = {
@@ -28,24 +29,81 @@ export default class CheckBox extends PureComponent {
 
     render() {
         const { className, isSwitch, onChange, isChecked, label, children } = this.props;
+        const classNames = {
+            switch: '',
+            switchDot: '',
+            box: '',
+            boxCheck: '',
+            label: '',
+            switchChecked: '',
+            switchDotChecked: '',
+            boxChecked: '',
+            boxCheckChecked: '',
+            labelChecked: ''
+        };
+
+        if (className) {
+            classNames.switch = classify(className, '__switch');
+            classNames.switchDot = classify(className, '__switch-dot');
+            classNames.box = classify(className, '__box');
+            classNames.boxCheck = classify(className, '__box-check');
+            classNames.label = classify(className, '__label');
+
+            if (isChecked) {
+                classNames.switchChecked = classify(className, '__switch--checked');
+                classNames.switchDotChecked = classify(className, '__switch-dot--checked');
+                classNames.boxChecked = classify(className, '__box--checked');
+                classNames.boxCheckChecked = classify(className, '__box-check--checked');
+                classNames.labelChecked = classify(className, '__label--checked');
+            }
+        }
 
         return (
             <div
                 className={`CheckBox ${className}`}
                 onClick={onChange && this.onChange}>
                 {isSwitch
-                    ? <div className={`CheckBox__switch ${isChecked ? 'CheckBox__switch--checked' : ''}`}>
-                        <div className={`CheckBox__switch-dot ${isChecked ? 'CheckBox__switch-dot--checked' : ''}`} />
+                    ? <div
+                        className={`
+                            CheckBox__switch
+                            ${isChecked ? 'CheckBox__switch--checked' : ''}
+                            ${classNames.switch}
+                            ${classNames.switchChecked}
+                        `}>
+                        <div
+                            className={`
+                                CheckBox__switch-dot
+                                ${isChecked ? 'CheckBox__switch-dot--checked' : ''}
+                                ${classNames.switchDot}
+                                ${classNames.switchDotChecked}
+                            `} />
                     </div>
-                    : <div className={`CheckBox__box ${isChecked ? 'CheckBox__box--checked' : ''}`}>
+                    : <div
+                        className={`
+                            CheckBox__box
+                            ${isChecked ? 'CheckBox__box--checked' : ''}
+                            ${classNames.box}
+                            ${classNames.boxChecked}
+                        `}>
                         <FontAwesomeIcon
-                            className={`CheckBox__box-check ${isChecked ? 'CheckBox__box-check--checked' : ''}`}
+                            className={`
+                                CheckBox__box-check
+                                ${isChecked ? 'CheckBox__box-check--checked' : ''}
+                                ${classNames.boxCheck}
+                                ${classNames.boxCheckChecked}
+                            `}
                             icon={faCheck} />
                     </div>
                 }
 
                 {children || (!!label && (
-                    <div className='CheckBox__label'>
+                    <div
+                        className={`
+                            CheckBox__label
+                            ${isChecked ? 'CheckBox__label--checked' : ''}
+                            ${classNames.label}
+                            ${classNames.labelChecked}
+                        `}>
                         {label}
                     </div>
                 ))}
